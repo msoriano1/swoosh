d4a4ef76866fbdf696bb8ae0e1acd61d8650a150
msoriano1 <matthew.soriano@obf.ateneo.edu>
2019-07-16 19:05:55 +0800

    lesson-02

--- app/src/main/java/com/mattdog/swoosh/WelcomeActivity.kt
+++ app/src/main/java/com/mattdog/swoosh/WelcomeActivity.kt
@@ -8,5 +8,8 @@ class WelcomeActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_welcome)
+        //To setup new Activity, right click the main folder under 'java', select 'New', select 'Activity', select 'Empty Activity'
+
+
     }
 }
